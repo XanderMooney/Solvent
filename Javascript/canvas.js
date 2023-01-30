41df90a488c1be32ee61d9867723445374ea9554
Xander Mooney <113313704+XanderMooney@users.noreply.github.com>
2023-01-30 10:45:14 -0500

    canvas movement start

--- Javascript/canvas.js
+++ Javascript/canvas.js
@@ -1,27 +1,26 @@
 const canvas = document.querySelector('#canvas')
 let ctx = canvas.getContext('2d')
 var scale = 1, xFrom = -10, xTo = 10, yFrom = -10, yTo = 10
-var mousedown = false, wasDown = false, dragging = false
+var mousedown = false, dragging = false
 var xMouse = 0, yMouse = 0
-const sensitivity = 150
+const sensitivity = 150, linesToDraw = 10
 var clientX = 0, clientY = 0
-
+var size = 2
 draw()
 addEventListener('resize', draw)
 
 // Change the scale whenever the user uses the mousewheel
-addEventListener('wheel', function() {
+addEventListener('wheel', function(event) {
     scale += event.deltaY * -0.01
 })
 
 canvas.addEventListener('mousedown', function() { mousedown = true })
 canvas.addEventListener('mouseup', function() { mousedown = false })
 
-canvas.addEventListener('mousemove', function() 
+canvas.addEventListener('mousemove', function(event) 
 {
     if (!mousedown)
     {
-        wasDown = false
         return
     } 
     clientX += event.movementX
@@ -47,11 +46,27 @@ function draw()
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight 
     
+    size = Math.round(Math.abs(xTo - xFrom)) / linesToDraw
+    
     ctx.strokeStyle = '#000000'
-
-    if (xFrom < 0 && xTo > 0)
+    
+    console.log(xTo + " " + xFrom)
+    for(let i = Math.floor(xFrom); i < xTo; ++i)
     {
-        let xZero = canvas.width * (Math.abs(xFrom) / (Math.abs(xFrom) + xTo))
+        if (i % size != 0)
+        {
+            continue
+        }
+
+        ctx.beginPath()
+        ctx.moveTo(xScreenPos(i), 0)
+        ctx.lineTo(xScreenPos(i), canvas.height)
+        ctx.stroke()
+
+    }
+    /* if (xFrom < 0 && xTo > 0)
+    {
+        let xZero = xScreenPos(0)
         ctx.beginPath()
         ctx.moveTo(xZero, 0)
         ctx.lineTo(xZero, canvas.height)
@@ -59,10 +74,18 @@ function draw()
     }
     if (yFrom < 0 && yTo > 0)
     {
-        let yZero = canvas.height * (Math.abs(yFrom) / (Math.abs(yFrom) + yTo))
+        let yZero = yScreenPos(0)
         ctx.beginPath()
         ctx.moveTo(0, yZero)
         ctx.lineTo(canvas.width, yZero)
         ctx.stroke()
-    }
+    } */
+}
+
+function xScreenPos(num) {
+    return canvas.width * ((Math.abs(xFrom) + num)/ (Math.abs(xFrom) + Math.abs(xTo)))
+}
+
+function yScreenPos(num) {
+    return canvas.height * ((Math.abs(yFrom) + num)/ (Math.abs(yFrom) + Math.abs(yTo)))
 }
